d4170e6e2c77088cca6883cdc0ca6f2ec5ad5b43
wanyulindev <wanyulin052@gmail.com>
2023-09-26 21:44:34 -0700

    Going through Dr.Angela's solution of Step 1.

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from twilio.rest import Client
 def day_time(day, hour):
     return datetime.datetime.combine((CURRENT_TIME - datetime.timedelta(days=day)).date(),
                                                   datetime.time(hour, 0, 0))
+# Since this doesn't work with every occasion:
+def date(day):
+    return (CURRENT_TIME - datetime.timedelta(days=day)).date()
 
 news_api_keys = os.environ.get("NEWS_API_KEYS")
 news_url = "https://newsapi.org/v2/everything"
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 CURRENT_TIME = datetime.datetime.now()
-print(CURRENT_TIME)
+# print(CURRENT_TIME)
 TWO_DAYS_AGO_MIDNIGHT = day_time(2, 0)
-print(TWO_DAYS_AGO_MIDNIGHT)
+# print(TWO_DAYS_AGO_MIDNIGHT)
 
 news_parameters = {
     "apiKey": news_api_keys,
@@ -27,7 +30,7 @@ news_response = requests.get(news_url, params=news_parameters)
 news_response.raise_for_status()
 news_data = news_response.json()
 # print(news_data)
-print(news_data["articles"][:3])
+# print(news_data["articles"][:3])
 # Step 2. done.
 #-------------------------------------------------------------------------------------------
 # Step 1.:
@@ -43,19 +46,72 @@ print(news_data["articles"][:3])
 # }
 #
 # stock_response = requests.get(stock_url, params=stock_parameters)
-stock_response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=60min&apikey=E21LGXJU3D3Q1VL7')
+stock_response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=E21LGXJU3D3Q1VL7')
 stock_response.raise_for_status()
-stock_data = stock_response.json()
+stock_data = stock_response.json()["Time Series (Daily)"]
 # print(stock_data)
 
-YESTERDAY_1900 = str(day_time(1, 19))
-DAY_BEFORE_YESTERDAY_1900 = str(day_time(2, 19))
+# This method will cause some problem: (ex: stock market only works on weekdays)
+# YESTERDAY = str(date(1))
+# DAY_BEFORE_YESTERDAY = str(date(2))
+# # print(stock_data["Time Series (Daily)"])
+# # print(stock_data["Time Series (Daily)"][YESTERDAY])
+#
+#
+# yesterday_data = stock_data["Time Series (Daily)"][YESTERDAY]
+# day_before_yesterday_data = stock_data["Time Series (Daily)"][DAY_BEFORE_YESTERDAY]
+# print(yesterday_data)
+# print(day_before_yesterday_data)
+
+# So, Let's try to change its type and iterate the value:
+stock_data_list = [value for key, value in stock_data.items()]
+# print(stock_data_list)
+yesterday_data = stock_data_list[0]
+# print(yesterday_data)
+yesterday_value= float(yesterday_data["4. close"])
+# print(yesterday_value)
+day_before_yesterday_value = float(stock_data_list[1]["4. close"])
+# print(day_before_yesterday_value)
+
+difference = abs(yesterday_value - day_before_yesterday_value)
+# print(difference)
+diff_percent = (difference / yesterday_value) * 100
+# print(diff_percent)
+if  diff_percent >= 5:
+    print(news_data["articles"][:3])
+
+
+
+
+
+
+
+
+# YESTERDAY_1900 = str(day_time(1, 19))
+# DAY_BEFORE_YESTERDAY_1900 = str(day_time(2, 19))
 # print(type(YESTERDAY_1900))
 # print(YESTERDAY_1900)
 
+# print(stock_data["Time Series (60min)"][YESTERDAY_1900])
+# Step 1. done
 
-print(stock_data["Time Series (60min)"][YESTERDAY_1900])
-
+# Here what I can approve my code:
+# stock_url = "https://www.alphavantage.co/query"
+# stock_api_keys = "E21LGXJU3D3Q1VL7"
+#
+# stock_parameters = {
+#     "function": "TIME_SERIES_DAILY",
+#     "symbol": STOCK,
+#     "apiKey": stock_api_keys
+# }
+#
+# stock_response = requests.get(stock_url, params=stock_parameters)
+# stock_response.raise_for_status()
+# stock_data = stock_response.json()
+# print(stock_data)
+# These doesn't work!:
+# {'Error Message': 'the parameter apikey is invalid or missing. Please claim your free API key on (https://www.alphavantage.co/support/#api-key). It should take less than 20 seconds.'}
+#---------------------------------------------------------------------------------------------
 
 
 
